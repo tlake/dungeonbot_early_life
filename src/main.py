db492dd926ca00e1d1f55e6be24b581f6154a776
Tanner Lake <tanner.lake@gmail.com>
2016-08-21 12:34:14 -0700

    begin bot testing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,40 @@
-import os
-from slacker import Slacker
+# import os
+# from slacker import Slacker
+
+
+# if __name__ == "__main__":
+#     pass
+
+
+# -*- coding: utf-8 -*-
+
+"""
+A tiny Flask app to verify the URL for Slack's Events API.
+"""
+
+from flask import Flask, Response, request
+from helpers import eprint
+
+
+################################
+# APP SETTINGS
+################################
+
+app = Flask(__name__)
+
+
+################################
+# API ROUTES
+################################
+
+@app.route("/", methods=["GET"])
+def verify():
+    eprint(dir(request))
+
+    return Response(
+        status=200,
+    )
 
 
 if __name__ == "__main__":
-    pass
+    app.run(host="0.0.0.0", port=5006)
